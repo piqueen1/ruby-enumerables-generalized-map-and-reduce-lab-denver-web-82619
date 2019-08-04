56e261ba8952f7520187c6e194a9cc1f0dda0a26
Finn Thye <piqueen@gmail.com>
2019-08-04 20:24:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,8 +15,12 @@ def reduce(source_array, starting_point=0)
   memo = starting_point
   
   while i < source_array.length do
-    if yield(source_array[i]).is_an(Integer)
-    memo += yield(source_array[i]) # <== Unique work
+    if yield(source_array[i]).is_a?(Integer)
+      memo += yield(source_array[i])
+    else
+      if yield(source_array[i]) == true
+    end
+    
     i += 1
   end
   
