10484a03fcfa01901c8e0f3f7a2e6e5391e750eb
Finn Thye <piqueen@gmail.com>
2019-08-04 18:20:21 +0000

    First commit on this lab

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,14 @@
 # Your Code Here
+def map_to_no_change(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] ) # <== Unique work
+    i += 1
+  end
+  return new
+end
+
+def map(source_array)
+
+end
